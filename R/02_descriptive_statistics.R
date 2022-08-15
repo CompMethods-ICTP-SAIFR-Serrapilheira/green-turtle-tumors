869894e2a30ebb3b8d569140ca7d1364fee5fd65
tawnunes <taw.ynunes@gmail.com>
2022-08-15 16:02:16 -0300

    Workflow for the descriptive statistics of the interest variables

---
+++ R/02_descriptive_statistics.R
@@ -0,0 +1,24 @@
+# Tumor Occurrence in Juvenile Green Turtles (Chelonia mydas) in Southern
+# Brazil.
+# 
+# Author: Tawane Yara Nunes
+# 
+# Data source: Laboratório de Ecologia e Conservação - UFPR
+# 
+# Description: 
+# The script allows to calculate the descriptive statistics of the 
+# interest variables
+# 
+# Requirements:
+# Run the 01_data_manipulation.R script
+
+
+# Sea turtles -----
+
+# How many individuals are being evaluated?
+length(green_turtle$id_turtle)
+
+## Developmental stage ----
+
+
+
